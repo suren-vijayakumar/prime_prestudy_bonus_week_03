68c2e5f0a85d17206495237ce110b7cab30f4605
suren-vijayakumar <SVijayakumar@LFW500.LPS.local>
2015-06-16 08:58:42 -0500

    Initial Commit

---
+++ prime_prestudy_bonus_week_03/prime_prestudy_bonus_week_03.js
@@ -0,0 +1,23 @@
+
+//prime_prestudy_bonus_week_03
+
+
+function A(x) {
+
+return x*2;
+
+}
+
+function B(n) {
+
+return n * 3;
+
+}
+
+function C(r) {
+
+return r*4 ;
+
+}
+
+console.log(A(B(C(2))));
